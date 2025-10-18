db6b332dfe2c1e9dc4cec92976c5a896dc9149ef
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-10-18 14:27:37 -0300

    Atualização do nome dos ID

--- PROJETO/Back_end/SQL/fbk.sql
+++ PROJETO/Back_end/SQL/fbk.sql
@@ -1,32 +1,30 @@
 create table Tecido (
-    id_tecido int primary key,
+    tecido_id int primary key,
     nome_tecido varchar(50) not null,
     cor varchar(20) not null,
     peso_metros float not null,
     composicao varchar(100) not null,
     gramatura float not null,
-    Fornecedor_id_fornecedor int,
-    foreign key (Fornecedor_id_fornecedor) references Fornecedor(id_fornecedor)
-    
+    fornecedor_id int,
+    foreign key (fornecedor_id) references Fornecedor(fornecedor_id)
 );
 
 create table Aviamento (
-    id_aviamento int primary key,
+    aviamento_id int primary key,
     tipo_aviamento varchar(50) not null,
     cor varchar(20) not null,
     peso_quantidade float not null,
-    composicao varchar(100) not null,
-    fabricante varchar(50) not null
+    composicao varchar(100) not null
 );
 
 create table Modelagem (
-    id_modelagem int primary key,
+    modelagem_id int primary key,
     tipo_molde varchar(50) not null,
-    Codigo_molde varchar(20) not null,
+    codigo_molde varchar(20) not null
 );
 
 create table Beneficiamento (
-    id_beneficiamento int primary key,
+    beneficiamento_id int primary key,
     digital boolean not null,
     bordado boolean not null,
     sublimacao boolean not null,
@@ -34,7 +32,7 @@ create table Beneficiamento (
 );
 
 create table Fornecedor (
-    id_fornecedor int primary key,
+    fornecedor_id int primary key,
     nome_fornecedor varchar(100) not null,
     razao_social varchar(100) not null,
     cnpj varchar(20) not null
