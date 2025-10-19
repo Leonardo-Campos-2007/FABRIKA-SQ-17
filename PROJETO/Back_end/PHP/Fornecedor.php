9f82952c12df9a07c9e93bd60e9ecb5b72c24461
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-10-19 15:58:43 -0300

    Get e set e função inserir de modelagem
    Get e set de Fornecedor

--- PROJETO/Back_end/PHP/Fornecedor.php
+++ PROJETO/Back_end/PHP/Fornecedor.php
@@ -12,6 +12,44 @@ Class Fornecedor {
         $this->nome_fornecedor = $nome_fornecedor;
         $this->razao_social = $razao_social;
         $this->cnpj = $cnpj;
-    }   
+    }  
+    
+    // ----------- GETTERS -----------
+
+    public function getFornecedorId() {
+        return $this->fornecedor_id;
+    }
+
+    public function getNomeFornecedor() {
+        return $this->nome_fornecedor;
+    }
+
+    public function getRazaoSocial() {
+        return $this->razao_social;
+    }
+
+    public function getCnpj() {
+        return $this->cnpj;
+    }
+
+    // ----------- SETTERS -----------
+
+    public function setFornecedorId($fornecedor_id) {
+        $this->fornecedor_id = $fornecedor_id;
+    }
+
+    public function setNomeFornecedor($nome_fornecedor) {
+        $this->nome_fornecedor = $nome_fornecedor;
+    }
+
+    public function setRazaoSocial($razao_social) {
+        $this->razao_social = $razao_social;
+    }
+
+    public function setCnpj($cnpj) {
+        $this->cnpj = $cnpj;
+    }
+
+    
 
 }
--- PROJETO/Back_end/PHP/Modelagem.php
+++ PROJETO/Back_end/PHP/Modelagem.php
@@ -1,14 +1,65 @@
-<?php
+    <?php
 
-class Modelagem {
-    private $modelagem_id;
-    private $tipo_molde;
-    private $codigo_molde;
+    class Modelagem {
+        private $modelagem_id;
+        private $tipo_molde;
+        private $codigo_molde;
 
-    function __construct($modelagem_id, $tipo_molde, $codigo_molde) {
-        $this->modelagem_id = $modelagem_id;
-        $this->tipo_molde = $tipo_molde;
-        $this->codigo_molde = $codigo_molde;
+        function __construct($modelagem_id, $tipo_molde, $codigo_molde) {
+            $this->modelagem_id = $modelagem_id;
+            $this->tipo_molde = $tipo_molde;
+            $this->codigo_molde = $codigo_molde;
+        }
+
+
+        // ----------- GETTERS -----------
+        public function getModelagemId() {
+            return $this->modelagem_id;
+        }
+
+        public function getTipoMolde() {
+            return $this->tipo_molde;
+        }
+
+        public function getCodigoMolde() {
+            return $this->codigo_molde;
+        }
+
+        // ----------- SETTERS -----------
+
+        public function setModelagemId($modelagem_id) {
+            $this->modelagem_id = $modelagem_id;
+        }
+
+        public function setTipoMolde($tipo_molde) {
+            $this->tipo_molde = $tipo_molde;
+        }
+
+        public function setCodigoMolde($codigo_molde) {
+            $this->codigo_molde = $codigo_molde;
+        }
+
+        public function inserirModelagem($conn) {
+            try{
+
+                $sql = "INSERT INTO modelagem (tipo_molde, codigo_molde) 
+                        VALUES (:tipo_molde, :codigo_molde)";
+
+
+                $stmt = $conn->prepare($sql);
+
+                $stmt->bindParam(':tipo_molde', $this->tipo_molde);
+                $stmt->bindParam(':codigo_molde', $this->codigo_molde);
+
+                if ($stmt->execute()) {
+                    echo "✅ Modelagem cadastrada com sucesso!";
+                } else {
+                    echo "❌ Erro ao cadastrar modelagem.";
+                }
+            } catch (PDOException $e) {
+                echo "Erro: " . $e->getMessage();
+            }
+        }
     }
 
-}
+    ?>
