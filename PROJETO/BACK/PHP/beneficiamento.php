659d4587bd70881428fc6a6df1b54dc09df01e41
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-11-09 12:59:15 -0300

    Função editar e deletar beneficiamento

--- PROJETO/BACK/PHP/beneficiamento.php
+++ PROJETO/BACK/PHP/beneficiamento.php
@@ -30,6 +30,59 @@
             }
         }
 
+        
+        public function editar(){
+            if (empty($this->id_beneficiamento)) {
+                return false;
+            }
+
+            $banco = new Banco();
+            $conn = $banco->conectar();
+
+            try {
+              
+
+                $stmt = $conn->prepare("UPDATE beneficiamento SET id_categoria = :id_categoria, descricao = :descricao WHERE id_beneficiamento = :id_beneficiamento");
+
+                $stmt->bindParam(':id_categoria', $this->id_categoria);
+                $stmt->bindParam(':descricao', $this->descricao);
+                $stmt->bindParam(':id_beneficiamento', $this->id_beneficiamento, PDO::PARAM_INT);
+
+                $result = $stmt->execute();
+
+                $banco->fecharConexao();
+
+                return $result;
+            } catch (PDOException $e) {
+                echo "Erro ao atualizar beneficiamento: " . $e->getMessage();
+                return false;
+            }
+        }
+
+       
+        public function deletar(){
+            if (empty($this->id_beneficiamento)) {
+                return false;
+            }
+
+            $banco = new Banco();
+            $conn = $banco->conectar();
+
+            try {
+                $stmt = $conn->prepare("DELETE FROM beneficiamento WHERE id_beneficiamento = :id_beneficiamento");
+                $stmt->bindParam(':id_beneficiamento', $this->id_beneficiamento, PDO::PARAM_INT);
+
+                $result = $stmt->execute();
+
+                $banco->fecharConexao();
+
+                return $result;
+            } catch (PDOException $e) {
+                echo "Erro ao deletar beneficiamento: " . $e->getMessage();
+                return false;
+            }
+        }
+
         function getIdBeneficiamento(){
             return $this->id_beneficiamento;
         }
