8aaaf79f20cfabf5c5daf86a490739ac199cc985
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-11-26 20:48:16 -0300

    Mudanças finais

--- Back_end/PHP/Conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $conn = new PDO("mysql:host=localhost;dbname=sistema_estoque", "root", "");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Erro na conexão: " . $e->getMessage();
-    exit;
-}
-?>
